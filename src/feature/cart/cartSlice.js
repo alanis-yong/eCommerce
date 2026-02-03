3d8417c98684a09473d7891e2dad933799e5b8b9
alanis-yong <alanisyongyr@gmail.com>
2026-02-03 20:24:33 +0800

    Initial commit

---
+++ src/feature/cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      // check if item is already in the cart
+      const itemIndex = state.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        // if item exists in the cart, increment the amount
+        state[itemIndex].amount += 1;
+      } else {
+        // if item does not exist in the cart, add it with amount: 1
+        const newProduct = { ...action.payload, amount: 1 };
+        console.log(newProduct);
+        state.push(newProduct);
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
